c2a8f0072f9b732e556d12d4dd979787efff0281
Dureti Shemsi <hamzadure@gmail.com>
2024-08-06 09:14:20 -0700

    still working on refrigerator challenege, done with the rest

--- sprint-1/11-encapsulation/exercise_intro-to-encapsulation/src/main/java/com/niantic/part_1_sakila_dtos/Actor.java
+++ sprint-1/11-encapsulation/exercise_intro-to-encapsulation/src/main/java/com/niantic/part_1_sakila_dtos/Actor.java
@@ -1,5 +1,45 @@
 package com.niantic.part_1_sakila_dtos;
 
-public class Actor
-{
+public class Actor {
+    private int ActorId;
+    private String FirstName;
+    private String LastName;
+
+    public Actor() {
+    }
+
+    public Actor(int actorId, String firstName, String lastName) {
+        ActorId = actorId;
+        FirstName = firstName;
+        LastName = lastName;
+    }
+
+    public int getActorId() {
+        return ActorId;
+    }
+
+    public void setActorId(int actorId) {
+        ActorId = actorId;
+    }
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getFullName(){
+        return STR."\{FirstName} \{LastName}";
+    }
+
 }
--- sprint-1/11-encapsulation/exercise_intro-to-encapsulation/src/main/java/com/niantic/part_2_objects/MathTest.java
+++ sprint-1/11-encapsulation/exercise_intro-to-encapsulation/src/main/java/com/niantic/part_2_objects/MathTest.java
@@ -1,5 +1,50 @@
 package com.niantic.part_2_objects;
 
-public class MathTest
-{
+public class MathTest {
+    private int score;
+    private int possiblePoints;
+    private String studentName;
+
+
+    public MathTest(int possiblePoints, String studentName) {
+        this.possiblePoints = possiblePoints;
+        this.studentName = studentName;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getPossiblePoints() {
+        return possiblePoints;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+    public int getPercent() {
+        double percent = (double) score / possiblePoints * 100;
+        return (int) percent;
+    }
+
+    public String getLetterGrade() {
+        double percent = getPercent();
+        if (percent >= 90) {
+            return "A";
+        } else if (percent >= 80) {
+            return "B";
+        } else if (percent >= 70) {
+            return "C";
+        } else if (percent >= 60) {
+            return "D";
+        } else {
+            return "F";
+        }
+    }
+
+
 }
