3594273e261ec081aed70e618b8632f575c1ebe7
Dureti Shemsi <hamzadure@gmail.com>
2024-08-13 07:54:41 -0700

    budget tracker and html page

--- sprint-1/14-review/budget-tracker/src/main/java/com/niantic/services/VendorDao.java
+++ sprint-1/14-review/budget-tracker/src/main/java/com/niantic/services/VendorDao.java
@@ -1,7 +1,11 @@
 package com.niantic.services;
 
+import com.niantic.models.Vendor;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+
+import java.util.ArrayList;
 
 public class VendorDao
 {
@@ -18,4 +22,121 @@ public class VendorDao
 
         jdbcTemplate = new JdbcTemplate(dataSource);
     }
+    public ArrayList<Vendor> getVendor()
+    {
+        // create the empty ArrayList
+        ArrayList<Vendor> vendors = new ArrayList<>();
+
+        // 1. build your query
+
+        String sql = """
+        SELECT vendor_id
+               , vendor_name
+               , website
+        FROM vendors;
+        """;
+
+        // 2. execute the query
+
+        SqlRowSet row = jdbcTemplate.queryForRowSet(sql);
+
+        // 3. loop through the rows
+
+        while(row.next()){
+            int vendorId = row.getInt("vendor_id");
+            String vendorName = row.getString("vendor_name");
+            String website = row.getString("website");
+
+            Vendor vendor = new Vendor(vendorId,vendorName,website);
+
+            vendors.add(vendor);
+        }
+
+        return vendors;
+    }
+
+    public Vendor getVendorById(int vendorId)
+    {
+        String sql = """
+        SELECT vendor_id
+               , vendor_name
+               , website
+        FROM vendors;
+        WHERE vendor_id = ?;
+        """;
+        var row = jdbcTemplate.queryForRowSet(sql, vendorId);
+
+        if(row.next())
+        {
+            int id = row.getInt("vendor_id");
+            String vendorName = row.getString("vendor_name");
+            String website = row.getString("website");
+
+            return new Vendor(id, vendorName, website);
+
+        }
+        return null;
+    }
+
+    public Vendor getVendorByName(String vendorName)
+    {
+        String sql = """
+        SELECT vendor_id
+               , vendor_name
+               , website
+        FROM vendors;
+        WHERE vendor_id = ?;
+        """;
+        var row = jdbcTemplate.queryForRowSet(sql, vendorName);
+
+        if(row.next())
+        {
+            int vendorId = row.getInt("vendor_id");
+            String name = row.getString("vendor_name");
+            String website = row.getString("website");
+
+            return new Vendor(vendorId, name, website);
+
+        }
+        return null;
+    }
+
+    public void addVendor (Vendor vendor)
+    {
+        String sql = """
+        INSERT INTO vendors
+            (vendor_name
+                , website)
+        VALUES
+        (?,?);
+        """;
+        jdbcTemplate.update(sql
+                , vendor.getVendorId()
+                , vendor.getWebsite()
+        );
+    }
+
+    public void updateVendor (Vendor vendor)
+    {
+        String sql = """
+        UPDATE vendors
+        SET vendor_name = ?
+               , website = ?
+        WHERE vendor_id = ?;
+        """;
+
+        jdbcTemplate.update(sql
+                , vendor.getVendorName()
+                , vendor.getWebsite()
+                , vendor.getVendorId()
+        );
+    }
+
+    public void deleteVendor (int vendorId)
+    {
+        String sql = "DELETE FROM categories WHERE vendor_id = ?;";
+
+        jdbcTemplate.update(sql, vendorId);
+    }
+
 }
