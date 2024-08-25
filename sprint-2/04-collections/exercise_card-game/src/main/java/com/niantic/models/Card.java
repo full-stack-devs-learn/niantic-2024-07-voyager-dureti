a0b7fc7a527320f033f6c40434b4e43d1767c6ca
Dureti Shemsi <hamzadure@gmail.com>
2024-08-25 15:13:42 -0700

    updated card class

--- sprint-2/04-collections/exercise_card-game/src/main/java/com/niantic/models/Card.java
+++ sprint-2/04-collections/exercise_card-game/src/main/java/com/niantic/models/Card.java
@@ -4,11 +4,13 @@ public class Card
 {
     private String color;
     private int number;
+    private boolean isFaceUp;
 
     public Card(String color, int number)
     {
         this.color = color;
         this.number = number;
+        this.isFaceUp = false;
     }
 
     public String getColor()
@@ -19,5 +21,17 @@ public class Card
     public int getValue()
     {
         return number;
+    }
+
+    public boolean isFaceUp() {
+        return isFaceUp;
+    }
+
+    public void flip() {
+        isFaceUp = !isFaceUp;
+    }
+    @Override
+    public String toString() {
+        return color + " " + number;
     }
 }
