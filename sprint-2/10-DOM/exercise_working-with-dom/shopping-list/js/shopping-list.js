10a43a9b7780e4100a3e4ad369648861b16c6aea
Dureti Shemsi <hamzadure@gmail.com>
2024-09-01 17:52:35 -0700

    completed DOM excercise

--- sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-list.js
+++ sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-list.js
@@ -1,26 +1,82 @@
 // add pageTitle
 
 // add groceries
-
+const service = new ShoppingService();
 /**
  * This function will get a reference to the title and set its text to the value
  * of the pageTitle variable that was set above.
  */
-function displayListTitle() {}
+function displayListTitle() {
+    const shoppingTitle = document.getElementById("title");
+    shoppingTitle.textContent = service.getListName();
+}
 
 /**
  * This function will loop over the array of groceries that was set above and add them to the DOM.
  */
-function displayGroceries() {}
+function displayGroceries() {
+    const groceries = service.getShoppingList();
+    const groceryContainer = document.getElementById("shopping-list");
 
+    groceries.forEach(grocery => {
+        createShoppingListDiv(grocery, groceryContainer);
+    });
+}
+
+function createShoppingListDiv(grocery, parent) {
+    const shoppingDiv = document.createElement("div");
+    shoppingDiv.classList.add("list-item");
+
+    // check if item marked complete, add to complete
+    if (grocery.isComplete) {
+        shoppingDiv.classList.add('complete');
+    }
+
+    parent.appendChild(shoppingDiv);
+
+    // add all inner div
+    buildItemTitle(grocery, shoppingDiv);
+    buildQuantity(grocery, shoppingDiv);
+}
+
+function buildItemTitle(grocery, parent) {
+    const itemTitleDiv = document.createElement("div");
+    itemTitleDiv.textContent = grocery.title;
+    parent.appendChild(itemTitleDiv);
+}
+
+function buildQuantityLabel(grocery, parent) {
+    const quantityLabel = document.createElement('span');
+    quantityLabel.classList.add('super');
+    quantityLabel.textContent = "quantity";
+    parent.appendChild(quantityLabel);
+}
+
+function buildQuantity(grocery, parent) {
+    const quantityDiv = document.createElement("div");
+    quantityDiv.classList.add("quantity-container");
+
+    buildQuantityLabel(grocery, quantityDiv);
+    const quantityText = document.createTextNode(grocery.quantity);
+    quantityDiv.appendChild(quantityText);
+
+    parent.appendChild(quantityDiv);
+}
 /**
  * This function will be called when the button is clicked. You will need to get a reference
  * to every list item and add the class completed to each one
  */
 function markCompleted() {
+    const items = document.querySelectorAll('.list-item');
+    items.forEach(item => {
+    item.classList.add('complete');
+    });
 }
 
+document.addEventListener("DOMContentLoaded", () => {
 
-displayListTitle();
-displayGroceries();
+    displayListTitle();
 
+    displayGroceries();
+
+});
--- sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-service.js
+++ sprint-2/10-DOM/exercise_working-with-dom/shopping-list/js/shopping-service.js
@@ -1,6 +1,7 @@
 class ShoppingService {
 
     getListName() {
+        return "Shopping Lists"
     }
 
     getShoppingList() {
@@ -15,6 +16,55 @@ class ShoppingService {
                 id: 2,
                 title: 'Frozen Pizza',
                 quantity: 4,
+                isComplete: true
+            },
+            {
+                id: 3,
+                title: '2 lb Tomato',
+                quantity: 1,
+                isComplete: false
+            },
+            {
+                id: 4,
+                title: '2 Bags of Chips',
+                quantity: 2,
+                isComplete: true
+    
+            },
+            {
+                id: 5,
+                title: 'Frozen Mixed Vegetables',
+                quantity: 2,
+                isComplete: false
+            },
+            {
+                id: 6,
+                title: '3 lb Ground Beef',
+                quantity: 1,
+                isComplete: false
+            },
+            {
+                id: 7,
+                title: '1 Gallon of Milk',
+                quantity: 1,
+                isComplete: false
+            },
+            {
+                id: 8,
+                title: 'Loaf of Bread',
+                quantity: 2,
+                isComplete: true
+            },
+            {
+                id: 9,
+                title: '4 Apples',
+                quantity: 4,
+                isComplete: false
+            },
+            {
+                id: 10,
+                title: 'Dozen Eggs',
+                quantity: 1,
                 isComplete: true
             }
         ];
