dc4086e41946bdfc1c7061f4d464212672f11f1b
Dureti Shemsi <hamzadure@gmail.com>
2024-09-19 22:11:56 -0700

    completed rest-api excercise

---
+++ sprint-3/06-rest-api/exercise_rest-api/src/main/java/com/niantic/controllers/apis/ProductsController.java
@@ -0,0 +1,43 @@
+package com.niantic.controllers.apis;
+
+import com.niantic.models.Product;
+import com.niantic.services.MySqlProductDao;
+import com.niantic.services.ProductDao;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ProductsController {
+
+    private ProductDao productDao = new MySqlProductDao();
+
+    @GetMapping("/api/products")
+    public List<Product> getProductsByCategory(@RequestParam int catId) {
+        return productDao.getProductsByCategory(catId);
+    }
+
+    @GetMapping("/api/products/{productId}")
+    public Product getProductById(@PathVariable int productId) {
+        return productDao.getProductById(productId);
+    }
+
+    @PostMapping("/api/products")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Product addProduct(@RequestBody Product product) {
+        return productDao.addProduct(product);
+    }
+
+    @PutMapping("/api/products/{productId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateProduct(@PathVariable int productId, @RequestBody Product product) {
+        productDao.updateProduct(productId, product);
+    }
+
+    @DeleteMapping("/api/products/{productId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteProduct(@PathVariable int productId) {
+        productDao.deleteProduct(productId);
+    }
+}
